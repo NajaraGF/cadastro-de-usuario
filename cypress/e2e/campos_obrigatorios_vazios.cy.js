e95be684d78f06efcef6d486ec6f7c9cfa5b7366
NajaraGF <najaguiland@gmail.com>
2025-02-08 19:02:15 -0300

    Atualização do código no HTML

--- cypress/e2e/campos_obrigatorios_vazios.cy.js
+++ cypress/e2e/campos_obrigatorios_vazios.cy.js
@@ -1,26 +1,30 @@
 describe('Validação de Campos Obrigatórios', () => {
   beforeEach(() => {
-    // Substitua pela URL correta do servidor local
+    // Substitua pela URL correta do seu servidor local
     cy.visit('http://127.0.0.1:5500/cadastro_de_usuario.index.html');
   });
 
   it('Exibe mensagens de erro ao deixar campos obrigatórios vazios', () => {
-    // Clique diretamente no botão de envio sem preencher os campos
+    // Clica no botão de enviar sem preencher nada
     cy.get('#submitButton').click();
 
-    // Verifica se as mensagens de erro são exibidas
-    cy.get('#nameError').should('be.visible').and('contain', 'O campo Nome Completo é obrigatório.');
-    // Adicione verificações similares para outros campos obrigatórios
-    cy.get('#birthdate').then(($input) => {
-      if ($input.prop('validationMessage')) {
-        expect($input.prop('validationMessage')).to.contain('Preencha este campo.');
-      }
-    });
+    // Verifica se as mensagens de erro são exibidas corretamente
+    cy.get('#nameError')
+      .and('contain', 'O campo Nome Completo é obrigatório.');
 
-    cy.get('#cep').then(($input) => {
-      if ($input.prop('validationMessage')) {
-        expect($input.prop('validationMessage')).to.contain('Preencha este campo.');
-      }
-    });
+    cy.get('#genderError')
+      .and('contain', 'Selecione uma opção de sexo.');
+
+    cy.get('#birthdateError')
+      .should('be.visible')
+      .and('contain', 'O campo Data de Nascimento é obrigatório.');
+
+    cy.get('#cepError')
+      .should('be.visible')
+      .and('contain', 'O campo CEP é obrigatório.');
+
+    cy.get('#emailError')
+      .should('be.visible')
+      .and('contain', 'O campo E-mail é obrigatório.');
   });
 });
