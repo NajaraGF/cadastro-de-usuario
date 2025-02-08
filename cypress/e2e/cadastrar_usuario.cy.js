d77a9e5a28550163d5a265dbeeb876d690e981fa
NajaraGF <najaguiland@gmail.com>
2025-02-08 18:37:17 -0300

    Ajuste na exibição da mensagem de sucesso

--- cypress/e2e/cadastrar_usuario.cy.js
+++ cypress/e2e/cadastrar_usuario.cy.js
@@ -1,22 +1,20 @@
 describe('Cadastro de Usuário', () => {
   beforeEach(() => {
-
-    cy.visit('http://127.0.0.1:8080/cadastro_de_usuario.html');
+    cy.visit('http://127.0.0.1:5500/cadastro_de_usuario.index.html');
   });
 
-  it('Cadastro bem-sucedido do usuário', () => {
-    // Preenche os campos do formulário
+  it('Verifica o alerta após o envio do formulário', () => {
     cy.get('#name').type('Maria Souza');
     cy.get('#genderFeminino').check();
     cy.get('#birthdate').type('1984-04-28');
-    cy.get('#cep').type('05011000'); // CEP sem hífen
+    cy.get('#cep').type('05011000');
     cy.get('#email').type('maria@teste.com');
     cy.get('#confirmEmail').type('maria@teste.com');
     cy.get('#password').type('Teste@123');
     cy.get('#confirmPassword').type('Teste@123');
 
-    // Submete o formulário e verifica o popup
     cy.get('#submitButton').click();
+
     cy.on('window:alert', (text) => {
       expect(text).to.equal('Cadastro realizado com sucesso!');
     });
