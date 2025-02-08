170267953d16801adbdf847da354b4d8a35d8a62
NajaraGF <najaguiland@gmail.com>
2025-02-08 18:50:30 -0300

    Ajuste no HTML

--- cypress/e2e/campos_obrigatorios_vazios.cy.js
+++ cypress/e2e/campos_obrigatorios_vazios.cy.js
@@ -1,36 +1,26 @@
-describe("Validar Cadastro de Usuário - Campos obrigatórios vazios", () => {
-    beforeEach(() => {
-      // Acessa a página inicial
-      cy.visit("https://www.guiademoteis.com.br/sao-paulo");
+describe('Validação de Campos Obrigatórios', () => {
+  beforeEach(() => {
+    // Substitua pela URL correta do servidor local
+    cy.visit('http://127.0.0.1:5500/cadastro_de_usuario.index.html');
+  });
+
+  it('Exibe mensagens de erro ao deixar campos obrigatórios vazios', () => {
+    // Clique diretamente no botão de envio sem preencher os campos
+    cy.get('#submitButton').click();
+
+    // Verifica se as mensagens de erro são exibidas
+    cy.get('#nameError').should('be.visible').and('contain', 'O campo Nome Completo é obrigatório.');
+    // Adicione verificações similares para outros campos obrigatórios
+    cy.get('#birthdate').then(($input) => {
+      if ($input.prop('validationMessage')) {
+        expect($input.prop('validationMessage')).to.contain('Preencha este campo.');
+      }
     });
-  
-    it("Deve exibir mensagens de erro quando os campos obrigatórios estiverem vazios", () => {
-      // Verifica se a página inicial foi carregada
-      cy.contains("Encontre os melhores motéis de São Paulo").should("be.visible");
-  
-      // Acessa o formulário de cadastro:
-      // Clica em "login" para abrir o modal de acesso
-      cy.get('#showModal').click();
-      cy.get('#login').click();
-      cy.wait(1000); // Aguarda transição/animação
-  
-      // No modal, clica em "Cadastre-se grátis!"
-      cy.get('#modal > p > a').click();
-      cy.wait(1000);
-  
-      // Tenta enviar o formulário deixando os campos obrigatórios vazios
-      cy.get('#cadastro > form > input.btCadastrar').click();
-      cy.wait(1000);
-  
-      // Valida a exibição das mensagens de erro para cada campo obrigatório
-      cy.contains("Nome Completo é obrigatório").should("be.visible");
-      cy.contains("Sexo é obrigatório").should("be.visible");
-      cy.contains("Data de Nascimento é obrigatória").should("be.visible");
-      cy.contains("CEP é obrigatório").should("be.visible");
-      cy.contains("E-mail é obrigatório").should("be.visible");
-      cy.contains("Confirmação de e-mail é obrigatória").should("be.visible");
-      cy.contains("Senha é obrigatória").should("be.visible");
-      cy.contains("Aceite os termos de uso e política de privacidade").should("be.visible");
+
+    cy.get('#cep').then(($input) => {
+      if ($input.prop('validationMessage')) {
+        expect($input.prop('validationMessage')).to.contain('Preencha este campo.');
+      }
     });
   });
-  
+});
